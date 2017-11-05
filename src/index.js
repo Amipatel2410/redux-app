740b08beb5229f6188ecc909570d81101c110fb3
amisha patel <ami.patel2410@gmail.com>
2017-11-05 15:05:44 -0500

    code to understand how redux works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -17,4 +17,63 @@ const userData = {
 
 
 ReactDOM.render(<App user={userData}/>, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker(); */
+
+
+let store = {
+    repos: [],
+    username: ""
+};
+
+const CHANGE_USERNAME =  'CHANGE_USERNAME';
+const FETCHED_REPOS =  'FETCHED_REPOS'
+
+const reducer = function (store, action) {
+    let newStore;
+
+    switch (action.type) {
+        case CHANGE_USERNAME:
+            newStore = Object.assign({}, store, {username: action.data.value});
+            break;
+        case FETCHED_REPOS:
+            newStore = Object.assign({}, store, {repos: action.data.value});
+            break;
+        default: 
+            newStore = store;
+    }
+
+    return newStore;
+}
+
+function changeUser(value) {
+    return {
+        type: CHANGE_USERNAME,
+        data: {
+            value
+        }
+    } 
+}
+
+store = reducer(store, changeUser("o"));
+console.log(store);
+
+store = reducer(store, changeUser("oc"));
+console.log(store);
+
+store = reducer(store, changeUser("oct"));
+console.log(store);
+
+store = reducer(store, {
+    type: FETCHED_REPOS,
+    data: {
+        value: [
+            {
+                "id": 18221276,
+                "name": "git-consortium",
+                "full_name": "octocat/git-consortium"                
+              }
+        ]
+    }
+});
+
+console.log(store);
